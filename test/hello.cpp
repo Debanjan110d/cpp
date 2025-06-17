f8fd86d7b8d8d4ed0a54a0d8a1fcfdb85f845596
Debanjan110d <debanjangamedu@gmail.com>
2025-06-17 22:25:18 +0530

    Add .gitignore and update C++ files for better organization and clarity

--- test/hello.cpp
+++ test/hello.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
 using namespace std;
+namespace huha{
+    void display(){
+        //Comment
+    }
+}
 
 int main(){
+    huha ::display();
     cout << "Hello Chai From Debanjan";
     return 0;
 }
---
+++ test/hello2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std:: cout;
+using std:: endl;
+
+int main(){
+    //This is one way of printing and suing standard output stream directive directly
+    // std::cout <<"Hello Chai From Debanjan" << std::endl;
+    cout <<"Hello Chai From Debanjan" << endl;
+    return 0;
+}
