eaeb61e1bfc7593ba18f56f7dab951066acc96f4
Debanjan110d <debanjangamedu@gmail.com>
2025-06-19 23:24:19 +0530

    Add main.cpp with basic C++ program structure

---
+++ 01_Var_Const/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int score;
+    score = 100;
+    int age = 10;
+    const int id = 001; 
+    cout <<  "Welcome to C++" << endl;
+    return 0;
+}
